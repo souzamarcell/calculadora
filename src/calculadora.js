13048fa70ed5800498a3b17b0eef609f5a7cac9a
Marcell Souza <manaus.prolam@gmail.com>
2023-01-27 13:08:31 -0400

    :tada: parte 13 componente

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react'
 import './calculadora.css'
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-} from 'react-bootstrap'
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 // import CalculadoraService from './calculadora.service';
 
 function Calculadora() {
   // const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
 
-  const [txtNumeros, setTxtNumeros] =
-    useState('0')
-  const [numero1, setNumero1] = useState('0')
-  const [numero2, setNumero2] = useState(null)
-  const [operacao, setOperacao] = useState(null)
+  const [txtNumeros, setTxtNumeros] = useState('0')
+  // const [numero1, setNumero1] = useState('0')
+  // const [numero2, setNumero2] = useState(null)
+  // const [operacao, setOperacao] = useState(null)
 
-  function adicionarNumero(numero) {}
+  function adicionarNumero(numero) {
+    // if (operacao === null) {
+    //   resultado = concatenarNumero(numero1, numero);
+    //   setNumero1(resultado);
+    // } else {
+    //   resultado = concatenarNumero(numero2, numero);
+    //   setNumero2(resultado);
+    // }
+    // setTxtNumeros(resultado);
+    setTxtNumeros(txtNumeros + numero)
+  }
 
-  function definirOperacao(op) {}
+  function definirOperacao(op) {
+    setTxtNumeros(op)
+  }
 
   return (
     <Container
@@ -43,55 +48,121 @@ function Calculadora() {
               name="txtNumeros"
               className="text-right"
               readOnly="readonly"
+              value={txtNumeros}
               data-testid="txtNumeros"
             />
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button variant="light">7</Button>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('7')}
+            >
+              7
+            </Button>
           </Col>
           <Col>
-            <Button variant="light">8</Button>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('8')}
+            >
+              8
+            </Button>
           </Col>
           <Col>
-            <Button variant="light">9</Button>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('9')}
+            >
+              9
+            </Button>
           </Col>
           <Col>
-            <Button variant="warning">/</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button variant="light">4</Button>
-          </Col>
-          <Col>
-            <Button variant="light">5</Button>
-          </Col>
-          <Col>
-            <Button variant="light">6</Button>
-          </Col>
-          <Col>
-            <Button variant="warning">*</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button variant="light">1</Button>
-          </Col>
-          <Col>
-            <Button variant="light">2</Button>
-          </Col>
-          <Col>
-            <Button variant="light">3</Button>
-          </Col>
-          <Col>
-            <Button variant="warning">-</Button>
+            <Button
+              variant="warning"
+              onClick={() => definirOperacao('/')}
+            >
+              /
+            </Button>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Button variant="light">0</Button>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('4')}
+            >
+              4
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('5')}
+            >
+              5
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('6')}
+            >
+              6
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="warning"
+              onClick={() => definirOperacao('*')}
+            >
+              *
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('1')}
+            >
+              1
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('2')}
+            >
+              2
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('3')}
+            >
+              3
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              variant="warning"
+              onClick={() => definirOperacao('-')}
+            >
+              -
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('0')}
+            >
+              0
+            </Button>
           </Col>
           <Col>
             <Button variant="light">.</Button>
@@ -100,7 +171,12 @@ function Calculadora() {
             <Button variant="success">=</Button>
           </Col>
           <Col>
-            <Button variant="warning">+</Button>
+            <Button
+              variant="warning"
+              onClick={() => definirOperacao('+')}
+            >
+              +
+            </Button>
           </Col>
         </Row>
       </Container>
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Calculadora from './calculadora';
 
 test('renders learn react link', () => {
   render(<Calculadora />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
