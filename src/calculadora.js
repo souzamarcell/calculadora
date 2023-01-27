dc7c9ed9de41564fa590f1c885dc4155c92d4031
Marcell Souza <manaus.prolam@gmail.com>
2023-01-27 14:09:29 -0400

    :tada: parte finally

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,30 +1,73 @@
 import React, { useState } from 'react'
 import './calculadora.css'
 import { Container, Row, Col, Button, Form } from 'react-bootstrap'
-// import CalculadoraService from './calculadora.service';
+import CalculadoraService from './calculadora.service'
 
 function Calculadora() {
-  // const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
+  const [
+    calcular,
+    concatenarNumero,
+    SOMA,
+    SUBTRACAO,
+    DIVISAO,
+    MULTIPLICACAO,
+  ] = CalculadoraService()
 
   const [txtNumeros, setTxtNumeros] = useState('0')
-  // const [numero1, setNumero1] = useState('0')
-  // const [numero2, setNumero2] = useState(null)
-  // const [operacao, setOperacao] = useState(null)
+  const [numero1, setNumero1] = useState('0')
+  const [numero2, setNumero2] = useState(null)
+  const [operacao, setOperacao] = useState(null)
 
   function adicionarNumero(numero) {
-    // if (operacao === null) {
-    //   resultado = concatenarNumero(numero1, numero);
-    //   setNumero1(resultado);
-    // } else {
-    //   resultado = concatenarNumero(numero2, numero);
-    //   setNumero2(resultado);
-    // }
-    // setTxtNumeros(resultado);
-    setTxtNumeros(txtNumeros + numero)
+    let resultado
+    if (operacao === null) {
+      resultado = concatenarNumero(numero1, numero)
+      setNumero1(resultado)
+    } else {
+      resultado = concatenarNumero(numero2, numero)
+      setNumero2(resultado)
+    }
+    setTxtNumeros(resultado)
+    // setTxtNumeros(txtNumeros + numero)
   }
 
   function definirOperacao(op) {
-    setTxtNumeros(op)
+    // apenas define a operação caso ela não exista
+    if (operacao === null) {
+      setOperacao(op)
+      return
+    }
+    // caso operação estiver definida e número 2 selecionado, realiza o cálculo da operação
+    if (numero2 !== null) {
+      const resultado = calcular(
+        parseFloat(numero1),
+        parseFloat(numero2),
+        operacao
+      )
+      setOperacao(op)
+      setNumero1(resultado.toString())
+      setNumero2(null)
+      setTxtNumeros(resultado.toString())
+    }
+  }
+
+  function acaoCalcular() {
+    if (numero2 === null) {
+      return
+    }
+    const resultado = calcular(
+      parseFloat(numero1),
+      parseFloat(numero2),
+      operacao
+    )
+    setTxtNumeros(resultado)
+  }
+
+  function limpar() {
+    setTxtNumeros('0')
+    setNumero1('0')
+    setNumero2(null)
+    setOperacao(null)
   }
 
   return (
@@ -40,7 +83,9 @@ function Calculadora() {
       <Container>
         <Row>
           <Col xs="3">
-            <Button variant="danger">C</Button>
+            <Button variant="danger" onClick={limpar}>
+              C
+            </Button>
           </Col>
           <Col xs="9">
             <Form.Control
@@ -81,7 +126,7 @@ function Calculadora() {
           <Col>
             <Button
               variant="warning"
-              onClick={() => definirOperacao('/')}
+              onClick={() => definirOperacao(DIVISAO)}
             >
               /
             </Button>
@@ -115,7 +160,7 @@ function Calculadora() {
           <Col>
             <Button
               variant="warning"
-              onClick={() => definirOperacao('*')}
+              onClick={() => definirOperacao(MULTIPLICACAO)}
             >
               *
             </Button>
@@ -149,7 +194,7 @@ function Calculadora() {
           <Col>
             <Button
               variant="warning"
-              onClick={() => definirOperacao('-')}
+              onClick={() => definirOperacao(SUBTRACAO)}
             >
               -
             </Button>
@@ -165,15 +210,22 @@ function Calculadora() {
             </Button>
           </Col>
           <Col>
-            <Button variant="light">.</Button>
+            <Button
+              variant="light"
+              onClick={() => adicionarNumero('.')}
+            >
+              .
+            </Button>
           </Col>
           <Col>
-            <Button variant="success">=</Button>
+            <Button variant="success" onClick={acaoCalcular}>
+              =
+            </Button>
           </Col>
           <Col>
             <Button
               variant="warning"
-              onClick={() => definirOperacao('+')}
+              onClick={() => definirOperacao(SOMA)}
             >
               +
             </Button>
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,10 +1,67 @@
-// import { render, screen } from '@testing-library/react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Calculadora from './calculadora'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-import Calculadora from './calculadora';
+describe('Calculadora', () => {
+  it('Deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Calculadora />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-test('renders learn react link', () => {
-  render(<Calculadora />);
-  // const linkElement = screen.getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+  it('Deve limpar o campo de números', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('5'))
+    fireEvent.click(getByText('C'))
+    expect(getByTestId('txtNumeros')).toHaveValue('0')
+  })
+
+  it('Deve somar 3 + 3 e obter 6', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('3'))
+    fireEvent.click(getByText('+'))
+    fireEvent.click(getByText('3'))
+    fireEvent.click(getByText('='))
+    expect(getByTestId('txtNumeros')).toHaveValue('6')
+  })
+
+  it('Deve subtrair 5 - 4 e obter 1', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('5'))
+    fireEvent.click(getByText('-'))
+    fireEvent.click(getByText('4'))
+    fireEvent.click(getByText('='))
+    expect(getByTestId('txtNumeros')).toHaveValue('1')
+  })
+
+  it('Deve dividir 8 / 4 e obter 2', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('8'))
+    fireEvent.click(getByText('/'))
+    fireEvent.click(getByText('4'))
+    fireEvent.click(getByText('='))
+    expect(getByTestId('txtNumeros')).toHaveValue('2')
+  })
+
+  it('Deve multiplicar 3 * 3 e obter 9', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('3'))
+    fireEvent.click(getByText('*'))
+    fireEvent.click(getByText('3'))
+    fireEvent.click(getByText('='))
+    expect(getByTestId('txtNumeros')).toHaveValue('9')
+  })
+
+  it('Deve somar 1.5 + 1 e obter 2.5', () => {
+    const { getByTestId, getByText } = render(<Calculadora />)
+    fireEvent.click(getByText('1'))
+    fireEvent.click(getByText('.'))
+    fireEvent.click(getByText('5'))
+    fireEvent.click(getByText('+'))
+    fireEvent.click(getByText('1'))
+    fireEvent.click(getByText('='))
+    expect(getByTestId('txtNumeros')).toHaveValue('2.5')
+  })
+})
